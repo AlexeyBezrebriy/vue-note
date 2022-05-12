102fcd1e78233326f2c1ebadb2962c56776ee329
AlexeyBezrebriy <97750536+AlexeyBezrebriy@users.noreply.github.com>
2022-05-12 23:33:07 +0300

    create nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,43 @@
-<script setup></script>
+<template>
+  <div class="header">
+    <nav class="menu">
+      <ul class="menu__items">
+        <li class="menu__item">Logo</li>
+        <li class="menu__item">Notes</li>
+        <li class="menu__item">Information</li>
+      </ul>
+    </nav>
+  </div>
+</template>
 
-<template></template>
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    methods: {},
+  }
+</script>
 
-<style></style>
+<style>
+  * {
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .header {
+    background-color: #42d392;
+    height: 2rem;
+  }
+  .menu__items {
+    height: 2rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    align-items: center;
+  }
+  .menu__item {
+    display: block;
+    width: 100px;
+  }
+</style>
